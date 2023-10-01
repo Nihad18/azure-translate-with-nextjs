09b0a7127f3fe2bd906b91abb3531d3f0af87175
Nihad Balakishiev <nihadbalakisiyev18@gmail.com>
2023-10-01 20:51:41 +0400

    added items-start class

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import TranslateBox from "@/components/TranslateBox";
 export default function Home() {
   return (
-    <main className='flex items-center justify-center min-h-screen'>
+    <main className='flex items-start md:items-center justify-center min-h-screen'>
       <TranslateBox />
     </main>
   );
