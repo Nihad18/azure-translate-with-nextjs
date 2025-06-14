450cb38c40c95ed45e016aabf3c0d9959e4e2970
Nihad Balakishiyev <86147986+Nihad18@users.noreply.github.com>
2025-06-14 13:54:35 +0400

    Update AnswerBox.tsx

--- src/components/AnswerBox.tsx
+++ src/components/AnswerBox.tsx
@@ -4,7 +4,7 @@ const AnswerBox: React.FC<IAnswersProps> = ({
   answer,
 }) => {
   return (
-    <div className='w-full md:w-[50%]'>
+    <div className='w-full md:w-[60%]'>
       <div className='w-full md:h-[300px] md:max-h-[300px] min-h-[50px] max-h-[200px] break-words overflow-auto border pt-3 pl-4 pr-10 '>
         {answer} {(translate === true && answer !== "") ? "..." : ""}
         {(answer === "" && translate === true) ? "Translates..." : ""}
